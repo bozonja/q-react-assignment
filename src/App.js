68d05db340de199a8a5864af761a6d61b5193cea
Josip Markovic <bozadesign@gmail.com>
2021-12-04 16:37:19 +0100

    Add simple UI

--- src/App.js
+++ src/App.js
@@ -2,9 +2,54 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header> Q Agency React Assignment</header>
-    </div>
+    <section className="wrapper">
+      <header className="header">
+        <div className="title-with-search">
+          <h1 className="fake-logo">Posts</h1>
+          <div className="search">
+            <div className="form-group">
+              <input type="text" placeholder="Search posts..." />
+            </div>
+          </div>
+        </div>
+
+        <nav>
+          <ul>
+            <li className="navigation-item">
+              <a href="#" className="navigation-item-link">
+                Home
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+
+      <main>
+        <h2 className="page-title">Blog posts</h2>
+        <section className="posts">
+          <article className="post">
+            <h2 className="post-title">Blog title</h2>
+            <p>Some random text</p>
+            <p>User: Zvone</p>
+          </article>
+          <article className="post">
+            <h2 className="post-title">Blog title</h2>
+            <p>Some random text</p>
+            <p>User: Zvone</p>
+          </article>
+          <article className="post">
+            <h2 className="post-title">Blog title</h2>
+            <p>Some random text</p>
+            <p>User: Zvone</p>
+          </article>
+          <article className="post">
+            <h2 className="post-title">Blog title</h2>
+            <p>Some random text</p>
+            <p>User: Zvone</p>
+          </article>
+        </section>
+      </main>
+    </section>
   );
 }
 
