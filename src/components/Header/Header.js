e9a235eed21ce8b77cc242745d7859396b68bdc1
Josip Markovic <bozadesign@gmail.com>
2021-12-11 09:11:19 +0100

    Add post method

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import PropTypes from "prop-types";
+import { useLocation } from "react-router-dom";
 
 //components
 import { Navigation } from "./Navigation";
@@ -12,20 +13,24 @@ const Header = ({ helloMessage, searchTerm, setSearchTerm }) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const location = useLocation();
+
   return (
     <header className="header">
       <div className="title-with-search">
         <h1 className="logo-text">Posts app</h1>
-        <div className="search">
-          <div className="form-group">
-            <input
-              type="text"
-              placeholder="Search posts..."
-              value={searchTerm}
-              onChange={(e) => setSearchTerm(e.target.value)}
-            />
+        {location.pathname === "/" && (
+          <div className="search">
+            <div className="form-group">
+              <input
+                type="text"
+                placeholder="Search posts..."
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+              />
+            </div>
           </div>
-        </div>
+        )}
       </div>
       <Navigation helloMessage={helloMessage} />
     </header>
--- src/components/Posts/CreatePost.js
+++ src/components/Posts/CreatePost.js
@@ -1,4 +1,5 @@
-import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 
 //components
@@ -10,10 +11,63 @@ const CreatePost = ({ helloMessage }) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [user, setUser] = useState("Zvone");
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const post = { title, body, user };
+
+    fetch("http://localhost:4000/data", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+      body: JSON.stringify(post),
+    }).then(() => {
+      alert("Post created");
+      navigate("/");
+    });
+  };
+
   return (
     <>
       <PageTitle title="Create post" helloMessage={helloMessage} />
-      Here goes create post content
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>Title</label>
+          <input
+            type="text"
+            placeholder="enter title"
+            required
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+
+        <div className="form-group">
+          <label>Text</label>
+          <textarea
+            required
+            value={body}
+            onChange={(e) => setBody(e.target.value)}
+          />
+        </div>
+
+        <div className="form-group">
+          <label>Written by:</label>
+          <select value={user} onChange={(e) => setUser(e.target.value)}>
+            <option value="Zvone">Zvone</option>
+            <option value="Petar">Petar</option>
+            <option value="Nina">Nina</option>
+          </select>
+        </div>
+
+        <button type="submit"> Create</button>
+      </form>
     </>
   );
 };
